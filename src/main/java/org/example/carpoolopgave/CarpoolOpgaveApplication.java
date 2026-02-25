a2549d770d3387b9ee993c67eb6fc9c570233d06
Melissabekirova04 <229076304+Melissabekirova04@users.noreply.github.com>
2026-02-25 09:59:10 +0100

    Initial commit

---
+++ src/main/java/org/example/carpoolopgave/CarpoolOpgaveApplication.java
@@ -0,0 +1,13 @@
+package org.example.carpoolopgave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarpoolOpgaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarpoolOpgaveApplication.class, args);
+	}
+
+}
